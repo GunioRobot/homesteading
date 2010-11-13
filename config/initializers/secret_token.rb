e96efcc6a5078d5407197435bd0625df069fa03f
Shane Becker <veganstraightedge@gmail.com>
2010-11-13 10:57:35 -0600

    deleted comments and stupid files

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,1 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
 Homesteading::Application.config.secret_token = 'a6cbeb4f8b08b9ff3f023f454340c2fd0bff7b7e6741be307b0849153be983def43da823a207a708039e7ec5696b54bb7625736eb983ec46653e3f9f5a03277f'
