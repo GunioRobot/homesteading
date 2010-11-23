63eb7b165ccbf3bdcece63e30e8eae38ec10f042
Shane Becker <veganstraightedge@gmail.com>
2010-11-23 00:39:57 -0800

    sample photo

--- app/controllers/samples_controller.rb
+++ app/controllers/samples_controller.rb
@@ -1,6 +1,14 @@
 class SamplesController < ApplicationController
+  # this hash constant is avoid using the database for the samples
   ACTIVITIES = {
     :note   => ["larb"],
-    :status => ["homesteading", "data", "website", "idea", "open web", "personal web", "snowflake"]
+    :status => ["homesteading", "data", "website", "idea", "open web", "personal web", "snowflake"],
+    :photo  => ["portland","nintendo","video games","shelf","nate","n8","8-bit","collection","shelf"]
   }
+
+  before_filter :set_tags
+
+  def set_tags
+    @tags = SamplesController::ACTIVITIES[action_name.to_sym]
+  end
 end
