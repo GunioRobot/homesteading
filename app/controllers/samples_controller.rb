38fd766ade1171bfb43ef1da96ab0ab0e27542a6
Shane Becker <veganstraightedge@gmail.com>
2010-11-22 21:01:38 -0800

    sample status

--- app/controllers/samples_controller.rb
+++ app/controllers/samples_controller.rb
@@ -1,2 +1,9 @@
 class SamplesController < ApplicationController
+  def note
+    @tags = ["homesteading", "data", "website", "idea", "open web", "personal web", "snowflake"]
+  end
+
+  def status
+    @tags = ["larb"]
+  end
 end
