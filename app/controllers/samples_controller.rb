b3d9b57dfb1cc6c2330b01792877cedd0ff01067
Shane Becker <veganstraightedge@gmail.com>
2010-11-23 20:59:59 -0800

    bookmark sample

--- app/controllers/samples_controller.rb
+++ app/controllers/samples_controller.rb
@@ -1,12 +1,13 @@
 class SamplesController < ApplicationController
   # this hash constant is avoid using the database for the samples
   ACTIVITIES = {
-    :note   => ["larb"],
-    :status => ["homesteading", "data", "website", "idea", "open web", "personal web", "snowflake"],
-    :photo  => ["portland","nintendo","video games","shelf","nate","n8","8-bit","collection","shelf"],
-    :video  => ["cat", "dog", "puppie", "pit bull", "bike house", "salt lake city"],
-    :audio  => ["bramble", "slc", "salt lake city", "james miska", "folk", "diy"],
-    :quote  => ["crimethinc", "anarchism", "without adjectives"]
+    :note     => ["larb"],
+    :status   => ["homesteading", "data", "website", "idea", "open web", "personal web", "snowflake"],
+    :photo    => ["portland","nintendo","video games","shelf","nate","n8","8-bit","collection","shelf"],
+    :video    => ["cat", "dog", "puppie", "pit bull", "bike house", "salt lake city"],
+    :audio    => ["bramble", "slc", "salt lake city", "james miska", "folk", "diy"],
+    :quote    => ["crimethinc", "anarchism", "without adjectives"],
+    :bookmark => ["arcade fire", "html5", "google", "chrome", "experiment", "toread", "private"]
   }
 
   before_filter :set_tags
