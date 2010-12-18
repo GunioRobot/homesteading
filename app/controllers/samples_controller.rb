692a8e48a283664ceaf66e074a623f9455d91fc5
Shane Becker <veganstraightedge@gmail.com>
2010-12-18 01:05:51 -0800

    added code sample post

--- app/controllers/samples_controller.rb
+++ app/controllers/samples_controller.rb
@@ -1,14 +1,15 @@
 class SamplesController < ApplicationController
   # this hash constant is avoid using the database for the samples
   ACTIVITIES = {
-    :note     => ["larb"],
-    :status   => ["homesteading", "data", "website", "idea", "open web", "personal web", "snowflake"],
-    :photo    => ["portland","nintendo","video games","shelf","nate","n8","8-bit","collection","shelf"],
-    :video    => ["cat", "dog", "puppie", "pit bull", "bike house", "salt lake city"],
     :audio    => ["bramble", "slc", "salt lake city", "james miska", "folk", "diy"],
-    :quote    => ["crimethinc", "anarchism", "without adjectives"],
     :bookmark => ["arcade fire", "html5", "google", "chrome", "experiment", "toread", "private"],
-    :list     => ["zombies", "28 days later", "stacey", "10 rules"]
+    :code     => ["ruby", "rails", "helper"],
+    :list     => ["zombies", "28 days later", "stacey", "10 rules"],
+    :note     => ["larb"],
+    :photo    => ["portland","nintendo","video games","shelf","nate","n8","8-bit","collection","shelf"],
+    :quote    => ["crimethinc", "anarchism", "without adjectives"],
+    :status   => ["homesteading", "data", "website", "idea", "open web", "personal web", "snowflake"],
+    :video    => ["cat", "dog", "puppie", "pit bull", "bike house", "salt lake city"]
   }
 
   before_filter :set_tags
