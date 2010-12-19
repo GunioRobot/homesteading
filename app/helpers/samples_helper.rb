bac930ca9367709e372e6ac063cff91a9020ecf1
Shane Becker <veganstraightedge@gmail.com>
2010-12-18 18:38:41 -0800

    made permalinks link_tos instead of raw html

--- app/helpers/samples_helper.rb
+++ app/helpers/samples_helper.rb
@@ -1,6 +1,6 @@
 module SamplesHelper
   # TEMP HACK this would be a real link to the actual post
-  def link_to_post_path(content_type)
+  def sample_post_path(content_type)
     "/samples/#{content_type}"
   end
 end
