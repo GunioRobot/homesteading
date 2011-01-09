fad81e3f4b5aa1f952ac188677e691721612bfe5
Shane Becker <veganstraightedge@gmail.com>
2011-01-08 22:08:04 -0800

    sample product view

--- app/controllers/samples_controller.rb
+++ app/controllers/samples_controller.rb
@@ -2,17 +2,18 @@ class SamplesController < ApplicationController
   # HACK everything in the SamplesController and samples views are just for illustration
   # this hash constant is avoid using the database for the samples
   ACTIVITIES = {
-    :audio    => ["bramble", "slc", "salt lake city", "james miska", "folk", "diy"],
-    :bookmark => ["arcade fire", "html5", "google", "chrome", "experiment", "toread", "private"],
-    :code     => ["ruby", "rails", "helper"],
-    :event    => ["godspeed", "music", "show", "live", "los angeles", "la"],
-    :list     => ["zombies", "28 days later", "stacey", "10 rules"],
+    :audio    => ["bramble","slc","salt lake city","james miska","folk","diy"],
+    :bookmark => ["arcade fire","html5","google","chrome","experiment","toread","private"],
+    :code     => ["ruby","rails","helper"],
+    :event    => ["godspeed","music","show","live","los angeles","la"],
+    :list     => ["zombies","28 days later","stacey","10 rules"],
     :note     => ["larb"],
     :photo    => ["portland","nintendo","video games","shelf","nate","n8","8-bit","collection","shelf"],
-    :quote    => ["crimethinc", "anarchism", "without adjectives"],
-    :review   => ["spider-man", "ultimate spider-man", "ultimate", "comic", "bendis", "bagley", "babe ruth", "hank aaron", "stan lee", "jack kirby"],
-    :status   => ["homesteading", "data", "website", "idea", "open web", "personal web", "snowflake"],
-    :video    => ["cat", "dog", "puppie", "pit bull", "bike house", "salt lake city"]
+    :product  => ["shirt","tshirt","forest","situationist","green","trees","cityscape"],
+    :quote    => ["crimethinc","anarchism","without adjectives"],
+    :review   => ["spider-man","ultimate spider-man","ultimate","comic","bendis","bagley","babe ruth","hank aaron","stan lee","jack kirby"],
+    :status   => ["homesteading","data","website","idea","open web","personal web","snowflake"],
+    :video    => ["cat","dog","puppie","pit bull","bike house","salt lake city"]
   }
 
   before_filter :set_tags
