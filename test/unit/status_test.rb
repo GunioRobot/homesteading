182c4fc365f6702e87d2e0ed97f63a49d0a8e819
Sarah Mei <sarahmei@gmail.com>
2011-04-06 20:39:26 -0700

    Status model, minitest runs

---
+++ test/unit/status_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+require 'minitest/autorun'
+
+class MiniTest::Spec < MiniTest::Unit::TestCase
+  describe Status do
+    it "is sane" do
+      true.must_equal true
+    end
+    it "is sane" do
+      true.must_equal true
+    end
+    it "is sane" do
+      true.must_equal true
+    end
+    it "is sane" do
+      true.must_equal true
+    end
+    it "is sane" do
+      true.must_equal true
+    end
+  end
+end
